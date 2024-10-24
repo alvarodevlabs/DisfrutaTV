5a9101ea44532439f7fd941bd824c2b7e0876719
pelisdev <curielyseguro@gmail.com>
2024-10-25 01:20:22 +0200

    Commit subida completa 23

--- backend/app/models.py
+++ backend/app/models.py
@@ -9,7 +9,7 @@ class User(db.Model):
     username = db.Column(db.String(64), unique=True, nullable=False)
     email = db.Column(db.String(120), unique=True, nullable=False)
     password_hash = db.Column(db.String(128))
-    role = db.Column(db.String(20), default='user')
+    role = db.Column(db.String(20), default='admin')
     created_at = db.Column(db.DateTime, default=datetime.utcnow)
 
     # Relaciones
